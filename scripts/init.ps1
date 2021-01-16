592bf98c8747a5f5a65c9742e0727a7ea9de9804
Hiroyuki Ikuno <sam2kaikaramegusuri@gmail.com>
2021-01-17 00:11:15 +0900

    pyinstallerビルド追加

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -3,3 +3,11 @@ cd web
 npm install
 npm run build
 cd ../
+
+# pyinstallerビルド
+git submodule update --init --recursive
+cd pyinstaller/bootloader
+pipenv run python ./waf all
+cd ../
+pipenv run python setup.py install
+cd ../
